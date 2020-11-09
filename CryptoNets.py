4179778095579aff74a117f5491ea3cd3012c6d3
Jaewoong Sim <jaewoong.sim@gmail.com>
2020-11-09 21:29:31 +0900

    add cryptonets; add timing code in torchseal.cpp

---
+++ CryptoNets.py
@@ -0,0 +1,32 @@
+import torchseal as ts
+import torchsealcpp as t
+
+## the original CryptoNets have 5 layers
+## CONV-SQUARE-POOL(scaled mean pooling)-SQUARE-OUPUT
+
+## the reported time in the original paper
+## Encoding+Encryption: 122 seconds
+## CNN inference: 570 seconds
+## Decryption+Decoding: 5 seconds
+class CryptoNets(ts.Module):
+  def __init__(self):
+    super(CryptoNets, self).__init__()
+    self.conv_layer = ts.Sequential(
+      ts.Conv2d(1, 5, 5, 2),  ## (29-5)/2+1 = 13, 13*13*5=845
+      ts.Square()
+    ) 
+    self.fc_layer = ts.Sequential(
+      ts.Linear(845, 100),
+      ts.Square(),
+      ts.Linear(100, 10)
+    )
+  def forward(self, x):
+    r = self.conv_layer(x)
+    f = ts.flatten(r)
+    return sefl.fc_layer(f)
+
+if __name__ == '__main__':
+  cnn = CryptoNets()
+  x = t.Tensor3(1, 29, 29) ## no padding but assume that the image size is 29x29 (not 28x28) 
+  t.fill3(x)
+  cnn(x)
--- src/torchseal.cpp
+++ src/torchseal.cpp
@@ -28,6 +28,8 @@
 #include "seal/serialization.h"
 #include "seal/valcheck.h"
 
+#include <chrono>
+
 using namespace seal;
 using namespace std;
 
@@ -39,21 +41,21 @@ unique_ptr<BatchEncoder> encoder;
 namespace seal {
 
 Ciphertext operator+(Ciphertext ct1, Ciphertext ct2) {
-	Ciphertext result;
-	evaluator->add(ct1, ct2, result);
-	return result;
+  Ciphertext result;
+  evaluator->add(ct1, ct2, result);
+  return result;
 }
 
 Ciphertext operator-(Ciphertext ct1, Ciphertext ct2) {
-	Ciphertext result;
-	evaluator->sub(ct1, ct2, result);
-	return result;
+  Ciphertext result;
+  evaluator->sub(ct1, ct2, result);
+  return result;
 }
 
 Ciphertext operator*(const Ciphertext ct1, const Ciphertext ct2) {
-	Ciphertext result;
-	evaluator->multiply(ct1, ct2, result);
-	return result;
+  Ciphertext result;
+  evaluator->multiply(ct1, ct2, result);
+  return result;
 }
 
 }
@@ -112,189 +114,219 @@ void fill3(Tensor<Ciphertext, 3>& t);
 void fill4(Tensor<Ciphertext, 4>& t);
 
 Tensor<Ciphertext, 1> flatten(Tensor<Ciphertext, 3> t) {
-	auto d = t.dimensions();
+  auto d = t.dimensions();
 
-	Eigen::array<int, 1> new_dim{{d[0]*d[1]*d[2]}};
-	Tensor<Ciphertext, 1> result = t.reshape(new_dim);
+  Eigen::array<int, 1> new_dim{{d[0]*d[1]*d[2]}};
+  Tensor<Ciphertext, 1> result = t.reshape(new_dim);
 
-	return result;
+  return result;
 }
 
 Ciphertext sum3d(Tensor<Ciphertext, 3> t) {
-	auto d = t.dimensions();
-	Plaintext pt;
-	Ciphertext base;
+  auto d = t.dimensions();
+  Plaintext pt;
+  Ciphertext base;
 
-	encoder->encode(vector<uint64_t>(8192, 0ULL), pt);
-	encryptor->encrypt(pt, base);
+  encoder->encode(vector<uint64_t>(8192, 0ULL), pt);
+  encryptor->encrypt(pt, base);
 
-	for(int i = 0; i < d[0]; i++)
-		for(int j = 0; j < d[1]; j++)
-			for(int k = 0; k < d[2]; k++)
-				evaluator->add_inplace(base, t(i, j, k));
+  for(int i = 0; i < d[0]; i++)
+    for(int j = 0; j < d[1]; j++)
+      for(int k = 0; k < d[2]; k++)
+        evaluator->add_inplace(base, t(i, j, k));
 
-	return base;
+  return base;
 }
 
 Ciphertext sum1d(Tensor<Ciphertext, 1> t) {
-	auto d = t.dimensions();
-	Plaintext pt;
-	Ciphertext base;
+  auto d = t.dimensions();
+  Plaintext pt;
+  Ciphertext base;
 
-	encoder->encode(vector<uint64_t>(8192, 0ULL), pt);
-	encryptor->encrypt(pt, base);
+  encoder->encode(vector<uint64_t>(8192, 0ULL), pt);
+  encryptor->encrypt(pt, base);
 
-	for(int i = 0; i < d[0]; i++)
-		evaluator->add_inplace(base, t(i));
+  for(int i = 0; i < d[0]; i++)
+    evaluator->add_inplace(base, t(i));
 
-	return base;
+  return base;
 }
 
 
 Tensor<Ciphertext, 3> conv(Tensor<Ciphertext, 3> act, Tensor<Ciphertext, 4> weight, int stride) {
-	auto weight_dimension = weight.dimensions();
-	auto act_dimension = act.dimensions();
-	size_t weight_output_channel_num = weight_dimension[0];
-	size_t act_input_channel_num = act_dimension[0];
-	
-	// weight's windows should be NxN not MxN
-	// calculate the result dimension
-	if ((act_dimension[1] - weight_dimension[2]) % stride != 0) {
-		std::cout << "Conv::Wrong stride!!!" << std::endl;
-	}
-	if ((act_dimension[2] - weight_dimension[3]) % stride != 0) {
-		std::cout << "Conv::Wrong stride!!!" << std::endl;
-	}
+  std::chrono::steady_clock::time_point ss = std::chrono::steady_clock::now();
+  auto weight_dimension = weight.dimensions();
+  auto act_dimension = act.dimensions();
+  size_t weight_output_channel_num = weight_dimension[0];
+  size_t act_input_channel_num = act_dimension[0];
+  
+  // weight's windows should be NxN not MxN
+  // calculate the result dimension
+  if ((act_dimension[1] - weight_dimension[2]) % stride != 0) {
+    std::cout << "Conv::Wrong stride!!!" << std::endl;
+  }
+  if ((act_dimension[2] - weight_dimension[3]) % stride != 0) {
+    std::cout << "Conv::Wrong stride!!!" << std::endl;
+  }
 
-	size_t result_height = (act_dimension[1] - weight_dimension[2])/stride + 1;
-	size_t result_width = (act_dimension[2] - weight_dimension[3])/stride + 1;
-	
-	Tensor<Ciphertext, 3> result(weight_output_channel_num, result_height, result_width);
+  size_t result_height = (act_dimension[1] - weight_dimension[2])/stride + 1;
+  size_t result_width = (act_dimension[2] - weight_dimension[3])/stride + 1;
+  
+  Tensor<Ciphertext, 3> result(weight_output_channel_num, result_height, result_width);
 
-	for(size_t i = 0; i < weight_output_channel_num; i++) {
-		for(size_t j = 0; j < result_height; j++) {
-			for(size_t k = 0; k < result_width; k++) {
-				Eigen::array<int, 3> act_start = {0, stride*j, stride*k};
-				Eigen::array<int, 3> act_size = \
-					{act_input_channel_num, weight_dimension[2], weight_dimension[3]};
-				Eigen::array<int, 4> weight_start = {i, 0, 0, 0};
-				Eigen::array<int, 4> weight_size = \
-					{1, act_input_channel_num, weight_dimension[2], weight_dimension[3]};
-				Tensor<Ciphertext, 3> act_partial = act.slice(act_start, act_size);
-				Tensor<Ciphertext, 4> weight_partial = weight.slice(weight_start, weight_size);
-				Eigen::array<int, 3> new_dim{{act_input_channel_num, weight_dimension[2], weight_dimension[3]}};
-				Tensor<Ciphertext, 3> weight_partial_reshape = weight_partial.reshape(new_dim);
-				Tensor<Ciphertext, 3> res = act_partial * weight_partial_reshape;
-				result(i, j, k) = sum3d(res);
-			}
-		}
-	}
+  for(size_t i = 0; i < weight_output_channel_num; i++) {
+    for(size_t j = 0; j < result_height; j++) {
+      for(size_t k = 0; k < result_width; k++) {
+        Eigen::array<int, 3> act_start = {0, stride*j, stride*k};
+        Eigen::array<int, 3> act_size = \
+          {act_input_channel_num, weight_dimension[2], weight_dimension[3]};
+        Eigen::array<int, 4> weight_start = {i, 0, 0, 0};
+        Eigen::array<int, 4> weight_size = \
+          {1, act_input_channel_num, weight_dimension[2], weight_dimension[3]};
+        Tensor<Ciphertext, 3> act_partial = act.slice(act_start, act_size);
+        Tensor<Ciphertext, 4> weight_partial = weight.slice(weight_start, weight_size);
+        Eigen::array<int, 3> new_dim{{act_input_channel_num, weight_dimension[2], weight_dimension[3]}};
+        Tensor<Ciphertext, 3> weight_partial_reshape = weight_partial.reshape(new_dim);
+        Tensor<Ciphertext, 3> res = act_partial * weight_partial_reshape;
+        result(i, j, k) = sum3d(res);
+      }
+    }
+  }
 
-	return result;
+  std::chrono::steady_clock::time_point ee = std::chrono::steady_clock::now();
+  std::cout << __func__ << " Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(ee-ss).count() << " ms\n";
+  return result;
 }
 
 Tensor<Ciphertext, 1> fc(Tensor<Ciphertext, 1> act, Tensor<Ciphertext, 2> weight) {
-	auto d = weight.dimensions();
-	size_t output_channel = d[1]; 
-	size_t act_len = d[0];
+  std::chrono::steady_clock::time_point ss = std::chrono::steady_clock::now();
+  auto d = weight.dimensions();
+  size_t output_channel = d[1]; 
+  size_t act_len = d[0];
 
-	Tensor<Ciphertext, 1> result(output_channel);
+  Tensor<Ciphertext, 1> result(output_channel);
 
-	for(int i = 0; i < output_channel; i++) {
-		Eigen::array<int, 2> weight_start = {0, i};
-		Eigen::array<int, 2> weight_size = {act_len, 1};
-		Tensor<Ciphertext, 2> weight_partial = weight.slice(weight_start, weight_size);
-		Eigen::array<int, 1> new_dim{{act_len}};
-		Tensor<Ciphertext, 1> weight_partial_reshape = weight_partial.reshape(new_dim);
-		Tensor<Ciphertext, 1> res = act * weight_partial_reshape;
-		result(i) = sum1d(res);
-	}
-	return result;
+  for(int i = 0; i < output_channel; i++) {
+    Eigen::array<int, 2> weight_start = {0, i};
+    Eigen::array<int, 2> weight_size = {act_len, 1};
+    Tensor<Ciphertext, 2> weight_partial = weight.slice(weight_start, weight_size);
+    Eigen::array<int, 1> new_dim{{act_len}};
+    Tensor<Ciphertext, 1> weight_partial_reshape = weight_partial.reshape(new_dim);
+    Tensor<Ciphertext, 1> res = act * weight_partial_reshape;
+    result(i) = sum1d(res);
+  }
+
+  std::chrono::steady_clock::time_point ee = std::chrono::steady_clock::now();
+  std::cout << __func__ << " Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(ee-ss).count() << " ms\n";
+  return result;
 }
 
 Tensor<Ciphertext, 3> square3(Tensor<Ciphertext, 3> t) {
-	return t*t;
+  std::chrono::steady_clock::time_point ss = std::chrono::steady_clock::now();
+  Tensor<Ciphertext, 3> result(t*t);
+  std::chrono::steady_clock::time_point ee = std::chrono::steady_clock::now();
+  std::cout << __func__ << " Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(ee-ss).count() << " ms\n";
+
+  return result;
 }
 
 Tensor<Ciphertext, 1> square1(Tensor<Ciphertext, 1> t) {
-	return t*t;
+  std::chrono::steady_clock::time_point ss = std::chrono::steady_clock::now();
+  Tensor<Ciphertext, 1> result(t*t);
+  std::chrono::steady_clock::time_point ee = std::chrono::steady_clock::now();
+  std::cout << __func__ << " Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(ee-ss).count() << " ms\n";
+
+  return result;
 }
 
 void fill4(Tensor<Ciphertext, 4>& t) {
-	Plaintext pt;
-	Ciphertext ct;
+  Plaintext pt;
+  Ciphertext ct;
 
-	encoder->encode(vector<uint64_t>(8192, 0ULL), pt);	
-	encryptor->encrypt(pt, ct);
+  encoder->encode(vector<uint64_t>(8192, 0ULL), pt);  
+  encryptor->encrypt(pt, ct);
 
-	auto d = t.dimensions();
-	for(int i = 0; i < d[0]; i++)
-		for(int j = 0; j < d[1]; j++)
-			for(int k = 0; k < d[2]; k++)
-				for(int l = 0; l < d[3]; l++)
-					t(i, j, k, l) = ct;
+  auto d = t.dimensions();
+  for(int i = 0; i < d[0]; i++)
+    for(int j = 0; j < d[1]; j++)
+      for(int k = 0; k < d[2]; k++)
+        for(int l = 0; l < d[3]; l++)
+          t(i, j, k, l) = ct;
 }
 
 void fill3(Tensor<Ciphertext, 3>& t) {
-	Plaintext pt;
-	Ciphertext ct;
+  std::chrono::steady_clock::time_point ss = std::chrono::steady_clock::now();
+  Plaintext pt;
+  Ciphertext ct;
 
-	encoder->encode(vector<uint64_t>(8192, 0ULL), pt);	
-	encryptor->encrypt(pt, ct);
+  encoder->encode(vector<uint64_t>(8192, 0ULL), pt);  
+  encryptor->encrypt(pt, ct);
 
-	auto d = t.dimensions();
-	for(int i = 0; i < d[0]; i++)
-		for(int j = 0; j < d[1]; j++)
-			for(int k = 0; k < d[2]; k++)
-				t(i, j, k) = ct;
+  auto d = t.dimensions();
+  for(int i = 0; i < d[0]; i++)
+    for(int j = 0; j < d[1]; j++)
+      for(int k = 0; k < d[2]; k++)
+        t(i, j, k) = ct;
+
+  std::chrono::steady_clock::time_point ee = std::chrono::steady_clock::now();
+  std::cout << __func__ << " Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(ee-ss).count() << " ms\n";
 }
 
 void fill2(Tensor<Ciphertext, 2>& t) {
-	Plaintext pt;
-	Ciphertext ct;
+  std::chrono::steady_clock::time_point ss = std::chrono::steady_clock::now();
 
-	encoder->encode(vector<uint64_t>(8192, 0ULL), pt);	
-	encryptor->encrypt(pt, ct);
+  Plaintext pt;
+  Ciphertext ct;
 
-	auto d = t.dimensions();
-	for(int i = 0; i < d[0]; i++)
-		for(int j = 0; j < d[1]; j++)
-			t(i, j) = ct;
+  encoder->encode(vector<uint64_t>(8192, 0ULL), pt);  
+  encryptor->encrypt(pt, ct);
+
+  auto d = t.dimensions();
+  for(int i = 0; i < d[0]; i++)
+    for(int j = 0; j < d[1]; j++)
+      t(i, j) = ct;
+
+  std::chrono::steady_clock::time_point ee = std::chrono::steady_clock::now();
+  std::cout << __func__ << " Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(ee-ss).count() << " ms\n";
 }
 
 void fill1(Tensor<Ciphertext, 1>& t) {
-	Plaintext pt;
-	Ciphertext ct;
+  std::chrono::steady_clock::time_point ss = std::chrono::steady_clock::now();
+  Plaintext pt;
+  Ciphertext ct;
 
-	encoder->encode(vector<uint64_t>(8192, 0ULL), pt);	
-	encryptor->encrypt(pt, ct);
+  encoder->encode(vector<uint64_t>(8192, 0ULL), pt);  
+  encryptor->encrypt(pt, ct);
 
-	auto d = t.dimensions();
-	for(int i = 0; i < d[0]; i++)
-		t(i) = ct;
+  auto d = t.dimensions();
+  for(int i = 0; i < d[0]; i++)
+    t(i) = ct;
+
+  std::chrono::steady_clock::time_point ee = std::chrono::steady_clock::now();
+  std::cout << __func__ << " Time: " << std::chrono::duration_cast<std::chrono::microseconds>(ee-ss).count() << " ms\n";
 }
 
 PYBIND11_MODULE(torchsealcpp, m) {
-	pybind11::class_<Tensor<Ciphertext, 1>>(m, "Tensor1")
-		.def(pybind11::init<>())
-		.def(pybind11::init<int>());
-	pybind11::class_<Tensor<Ciphertext, 2>>(m, "Tensor2")
-		.def(pybind11::init<>())
-		.def(pybind11::init<int, int>());
-	pybind11::class_<Tensor<Ciphertext, 3>>(m, "Tensor3")
-		.def(pybind11::init<>())
-		.def(pybind11::init<int, int, int>());
-	pybind11::class_<Tensor<Ciphertext, 4>>(m, "Tensor4")
-		.def(pybind11::init<>())
-		.def(pybind11::init<int, int, int, int>());
-	m.def("init", &init);
-	m.def("conv", &conv);
-	m.def("flatten", &flatten);
-	m.def("fc", &fc);
-	m.def("fill1", &fill1);
-	m.def("fill2", &fill2);
-	m.def("fill3", &fill3);
-	m.def("fill4", &fill4);
-	m.def("square1", &square1);
-	m.def("square3", &square3);
+  pybind11::class_<Tensor<Ciphertext, 1>>(m, "Tensor1")
+    .def(pybind11::init<>())
+    .def(pybind11::init<int>());
+  pybind11::class_<Tensor<Ciphertext, 2>>(m, "Tensor2")
+    .def(pybind11::init<>())
+    .def(pybind11::init<int, int>());
+  pybind11::class_<Tensor<Ciphertext, 3>>(m, "Tensor3")
+    .def(pybind11::init<>())
+    .def(pybind11::init<int, int, int>());
+  pybind11::class_<Tensor<Ciphertext, 4>>(m, "Tensor4")
+    .def(pybind11::init<>())
+    .def(pybind11::init<int, int, int, int>());
+  m.def("init", &init);
+  m.def("conv", &conv);
+  m.def("flatten", &flatten);
+  m.def("fc", &fc);
+  m.def("fill1", &fill1);
+  m.def("fill2", &fill2);
+  m.def("fill3", &fill3);
+  m.def("fill4", &fill4);
+  m.def("square1", &square1);
+  m.def("square3", &square3);
 }
